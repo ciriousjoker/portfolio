eb4356f60e7a2434b5704b4a0b63832840c7c323
ciriousjoker <ciriousjoker@gmail.com>
2022-02-07 08:28:15 +0100

    Updated dependencies, regenerated models.

--- lib/models/project.model.g.dart
+++ lib/models/project.model.g.dart
@@ -6,27 +6,30 @@ part of 'project.model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-ProjectModel _$ProjectModelFromJson(Map<String, dynamic> json) {
-  return ProjectModel(
-    title: json['title'] as String,
-    timestamp: json['timestamp'] as int,
-    icon: json['icon'] as String,
-    githubName: json['githubName'] as String?,
-    description:
-        (json['description'] as List<dynamic>).map((e) => e as String).toList(),
-    learningsTldr: json['learningsTldr'] as String,
-    learnings:
-        (json['learnings'] as List<dynamic>).map((e) => e as String).toList(),
-    urlImage: json['urlImage'] as String?,
-    urlDemo: json['urlDemo'] as String?,
-    urlPlaystore: json['urlPlaystore'] as String?,
-    urlDownload: json['urlDownload'] as String?,
-    urlWebsite: json['urlWebsite'] as String?,
-    platforms:
-        (json['platforms'] as List<dynamic>).map((e) => e as String).toList(),
-    tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
-  );
-}
+ProjectModel _$ProjectModelFromJson(Map<String, dynamic> json) => ProjectModel(
+      title: json['title'] as String,
+      timestamp: json['timestamp'] as int,
+      icon: json['icon'] as String,
+      githubName: json['githubName'] as String?,
+      description: (json['description'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      learningsTldr: json['learningsTldr'] as String,
+      learnings:
+          (json['learnings'] as List<dynamic>).map((e) => e as String).toList(),
+      urlImage: json['urlImage'] as String?,
+      urlDemo: json['urlDemo'] as String?,
+      urlPlaystore: json['urlPlaystore'] as String?,
+      urlDownload: json['urlDownload'] as String?,
+      urlWebsite: json['urlWebsite'] as String?,
+      platforms: (json['platforms'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const [],
+      tags:
+          (json['tags'] as List<dynamic>?)?.map((e) => e as String).toList() ??
+              const [],
+    );
 
 Map<String, dynamic> _$ProjectModelToJson(ProjectModel instance) =>
     <String, dynamic>{
--- lib/models/projects.model.g.dart
+++ lib/models/projects.model.g.dart
@@ -6,13 +6,12 @@ part of 'projects.model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-ProjectsModel _$ProjectsModelFromJson(Map<String, dynamic> json) {
-  return ProjectsModel(
-    projects: (json['projects'] as List<dynamic>)
-        .map((e) => ProjectModel.fromJson(e as Map<String, dynamic>))
-        .toList(),
-  );
-}
+ProjectsModel _$ProjectsModelFromJson(Map<String, dynamic> json) =>
+    ProjectsModel(
+      projects: (json['projects'] as List<dynamic>)
+          .map((e) => ProjectModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
 
 Map<String, dynamic> _$ProjectsModelToJson(ProjectsModel instance) =>
     <String, dynamic>{
