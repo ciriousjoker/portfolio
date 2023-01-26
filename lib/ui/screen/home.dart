01b34fd6b3dc641d8fd2b6e9416f043da1caabdd
Philipp Bauer <ciriousjoker@gmail.com>
2023-01-26 16:31:07 +0100

    Remove SmoothScrollHelper because Flutter finally fixed the default scrolling

--- lib/ui/screen/home.dart
+++ lib/ui/screen/home.dart
@@ -1,8 +1,5 @@
-import "dart:ui";
-
 import "package:delayed_display/delayed_display.dart";
 import "package:flutter/material.dart";
-import "package:portfolio/ui/helper/smoothscroll.helper.dart";
 import "package:portfolio/ui/helper/ui.helper.dart";
 import "package:portfolio/ui/widget/card/card.widget.dart";
 import "package:portfolio/ui/widget/timeline/timeline.widget.dart";
@@ -16,10 +13,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _controller = ScrollController();
-
-  PointerDeviceKind _pointerKind = PointerDeviceKind.touch;
-
-  bool get _isMouse => _pointerKind == PointerDeviceKind.mouse;
 
   final card = DelayedDisplay(
     fadingDuration: const Duration(seconds: 2),
@@ -35,59 +28,48 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SmoothScrollWeb(
-      curve: Curves.easeOut,
-      onDeviceTypeChange: (value) {
-        setState(() {
-          _pointerKind = value;
-        });
-      },
-      controller: _controller,
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: kVerticalSpaceMedium,
-          ),
-          child: isVertical(context)
-              ? CustomScrollView(
-                  physics: _isMouse ? const NeverScrollableScrollPhysics() : null,
-                  controller: _controller,
-                  shrinkWrap: true,
-                  clipBehavior: Clip.none,
-                  slivers: <Widget>[
-                    SliverList(
-                      delegate: SliverChildListDelegate(
-                        [
-                          verticalSpaceMedium,
-                          card,
-                          verticalSpaceMedium,
-                        ],
-                      ),
-                    ),
-                    const TimelineWidget(),
-                  ],
-                )
-              : Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    horizontalSpaceMedium,
-                    card,
-                    horizontalSpaceMedium,
-                    Expanded(
-                      child: _delayed(
-                        CustomScrollView(
-                          physics: _isMouse ? const NeverScrollableScrollPhysics() : null,
-                          controller: _controller,
-                          clipBehavior: Clip.none,
-                          shrinkWrap: true,
-                          slivers: const <Widget>[TimelineWidget()],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kVerticalSpaceMedium,
         ),
+        child: isVertical(context)
+            ? CustomScrollView(
+                controller: _controller,
+                shrinkWrap: true,
+                clipBehavior: Clip.none,
+                slivers: <Widget>[
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        verticalSpaceMedium,
+                        card,
+                        verticalSpaceMedium,
+                      ],
+                    ),
+                  ),
+                  const TimelineWidget(),
+                ],
+              )
+            : Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  horizontalSpaceMedium,
+                  card,
+                  horizontalSpaceMedium,
+                  Expanded(
+                    child: _delayed(
+                      CustomScrollView(
+                        controller: _controller,
+                        clipBehavior: Clip.none,
+                        shrinkWrap: true,
+                        slivers: const <Widget>[TimelineWidget()],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
