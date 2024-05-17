40b873b8f86ce0f52bcc222962e4fdb98ef90e5e
Philipp Bauer <ciriousjoker@gmail.com>
2024-05-17 19:21:22 +0200

    Fix new linter warnings

--- lib/ui/widget/card/card.widget.dart
+++ lib/ui/widget/card/card.widget.dart
@@ -162,7 +162,7 @@ class _CardWidgetState extends State<CardWidget> {
                       }
                     },
                   ),
-                )
+                ),
               ],
             ),
           ),
