12fad76138c02f0dd98a45a2efbac90aca7457d3
Philipp Bauer <ciriousjoker@gmail.com>
2025-06-19 23:10:42 +0200

    Bump dependencies

--- lib/ui/widget/util/markdown_wrapper.widget.dart
+++ lib/ui/widget/util/markdown_wrapper.widget.dart
@@ -1,6 +1,6 @@
 import "package:flutter/gestures.dart";
 import "package:flutter/material.dart";
-import "package:flutter_markdown/flutter_markdown.dart";
+import "package:flutter_markdown_plus/flutter_markdown_plus.dart";
 import "package:markdown/markdown.dart" as md;
 import "package:portfolio/config/colors.config.dart";
 import "package:url_launcher/url_launcher_string.dart";
